724ea0ae28a50313bb3c9983dfc5ca90dd37766f
AhnYoungBin <49204525+AhnYoungBin@users.noreply.github.com>
2020-03-11 00:40:06 +0900

    Update ex.py

--- model/ex.py
+++ model/ex.py
@@ -1,1 +1,6 @@
-print('hello')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("square", help="display a square of a given number",
+                    type=int)
+args = parser.parse_args()
+print(args.square**2)
