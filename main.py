87c3f84336d83942c09a607a1dacf89e454c5304
AhnYoungBin <49204525+AhnYoungBin@users.noreply.github.com>
2020-03-18 23:54:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,10 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
 best_acc = 0
-
+train_loss_list =[]
+val_loss_list = []
+train_acc_list = []
+val_acc_list = []
 def train():
     model.train()
     train_loss = 0
@@ -75,7 +78,11 @@ def train():
 
     epoch_loss = train_loss /len(train_data_loader)
     epoch_acc = correct/ total
-    print('>> train | Loss : {.4f}  Acc : {.4f}'.format(epoch_loss,epoch_acc))
+
+    train_loss_list.append(epoch_loss)
+    train_acc_list.append(epoch_acc)
+
+    print('>> train | Loss : {:.4f}  Acc : {:.4f}'.format(epoch_loss,epoch_acc))
 def val():
     model.eval()
     val_loss = 0
@@ -95,11 +102,66 @@ def val():
             correct += predicted.eq(labels).sum().item()
     epoch_loss = val_loss /len(val_data_loader)
     epoch_acc = correct / total
-    print('>> test | Loss : {.4f}  Acc : {.4f}'.format(epoch_loss,epoch_acc))
+
+    val_loss_list.append(epoch_loss)
+    val_acc_list.append(epoch_acc)
+
+    print('>> test | Loss : {:.4f}  Acc : {:.4f}'.format(epoch_loss,epoch_acc))
 
 for epoch in range(1,args.epochs+1):
     print('Epoch {}/{}'.format(epoch, args.epochs))
     print('-'*20)
     train()
     val()
+
+
+epochs_range = range(args.epochs)
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, train_acc_list, label='Training Accuracy')
+plt.plot(epochs_range, val_acc_list, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, train_loss_list, label='Training Loss')
+plt.plot(epochs_range, val_loss_list, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+
+test_loss_list = []
+test_acc_list = []
+predicted_list = []
+label_list = []
+def test():
+    model.eval()
+    test_loss = 0
+    total = 0
+    correct = 0
+    for idx, (inputs, labels) in enumerate(test_data_loader):
+        inputs = inputs.to(device, dtype=torch.float)
+        labels = labels.to(device, dtype=torch.long)
+        with torch.no_grad():
+            outputs = model(inputs)
+            _, predicted = outputs.max(1)
+            loss = criterion(outputs, labels.squeeze(-1))
+            test_loss += loss.data.cpu().numpy()
+
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+
+            predicted_list.append(predicted)
+            label_list.append(labels)
+
+    epoch_loss = test_loss /len(test_data_loader)
+    epoch_acc = correct / total
+
+    test_loss_list.append(epoch_loss)
+    test_acc_list.append(epoch_acc)
+    print('-'*30)
+    print('>> test | Loss : {:.4f}  Acc : {:.4f}'.format(epoch_loss,epoch_acc))
+
+test()
 
