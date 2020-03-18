61f8bd703b06c902c2d5d2d994e47185559b4d26
AhnYoungBin <49204525+AhnYoungBin@users.noreply.github.com>
2020-03-19 00:18:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 import data
 import argparse
 import modellist
+import matplotlib.pyplot as plt
 
 PATH = '/content/gdrive/My Drive/dataset'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
