96bb724e94478ed6f20a4149b3494bff7e721edb
AhnYoungBin <49204525+AhnYoungBin@users.noreply.github.com>
2020-03-19 23:47:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import data
 import argparse
 import modellist
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-from mlxtend.plotting import plot_confusion_matrix
 import os
+import utils
 
 PATH = '/content/gdrive/My Drive/dataset'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -47,17 +46,19 @@ val_data_loader = DataLoader(Val, batch_size = 16, shuffle=False, num_workers=2)
 test_data_loader = DataLoader(Test, batch_size = 16, shuffle=False, num_workers=2)
 
 model = modellist(args.modelnum, seon = args.se)
+
+#show to model archtecture
 if args.show:
     print(model)
+
 model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
 best_acc = 0
-train_loss_list =[]
-val_loss_list = []
-train_acc_list = []
-val_acc_list = []
+
+history_dict = {'train':{'acc':[],'loss':[]},'val':{'acc':[],'loss':[]}}
+
 def train():
     model.train()
     train_loss = 0
@@ -83,11 +84,13 @@ def train():
     epoch_loss = train_loss /len(train_data_loader)
     epoch_acc = correct/ total
 
-    train_loss_list.append(epoch_loss)
-    train_acc_list.append(epoch_acc)
+    history_dict['train']['loss'].append(epoch_loss)
+    history_dict['train']['acc'].append(epoch_acc)
 
     print('>> train | Loss : {:.4f}  Acc : {:.4f}'.format(epoch_loss,epoch_acc))
+
 def val():
+    global best_acc
     model.eval()
     val_loss = 0
     total = 0
@@ -107,74 +110,27 @@ def val():
     epoch_loss = val_loss /len(val_data_loader)
     epoch_acc = correct / total
 
-    val_loss_list.append(epoch_loss)
-    val_acc_list.append(epoch_acc)
+    history_dict['val']['loss'].append(epoch_loss)
+    history_dict['val']['acc'].append(epoch_acc)
 
-    print('>> test | Loss : {:.4f}  Acc : {:.4f}'.format(epoch_loss,epoch_acc))
+    print('>> val | Loss : {:.4f}  Acc : {:.4f}'.format(epoch_loss,epoch_acc))
+    #save model
+    if epoch_acc > best_acc:
+        best_acc = epoch_acc
+        if best_acc > 70:
+            savePath = "/content/ex/model/trainmodel.pth"
+            torch.save(model.state_dict(), savePath)
 
+#training model
 for epoch in range(1,args.epochs+1):
     print('Epoch {}/{}'.format(epoch, args.epochs))
     print('-'*20)
     train()
     val()
 
-try:
-    if not os.path.exists('/content/ex/workspace'):
-        os.makedirs('/content/ex/workspace')
-except OSError:
-    print('Error Creating director')
+#save to traingraph
+utils.train_graph(args.epochs,history_dict)
 
-epochs_range = range(args.epochs)
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, train_acc_list, label='Training Accuracy')
-plt.plot(epochs_range, val_acc_list, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, train_loss_list, label='Training Loss')
-plt.plot(epochs_range, val_loss_list, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.savefig('/content/ex/workspace/train_val_graph.png', dpi=300)
-
-test_loss_list = []
-test_acc_list = []
-predicted_list = []
-label_list = []
-def test():
-    model.eval()
-    test_loss = 0
-    total = 0
-    correct = 0
-    for idx, (inputs, labels) in enumerate(test_data_loader):
-        inputs = inputs.to(device, dtype=torch.float)
-        labels = labels.to(device, dtype=torch.long)
-        with torch.no_grad():
-            outputs = model(inputs)
-            _, predicted = outputs.max(1)
-            loss = criterion(outputs, labels.squeeze(-1))
-            test_loss += loss.data.cpu().numpy()
-
-            total += labels.size(0)
-            correct += predicted.eq(labels).sum().item()
-
-            predicted_list.append(predicted)
-            label_list.append(labels)
-
-    epoch_loss = test_loss /len(test_data_loader)
-    epoch_acc = correct / total
-
-    cm = confusion_matrix(predicted_list,predicted_list)
-    fig, ax = plot_confusion_matrix(conf_mat=cm,figsize=(8,8))
-    ax.set_xticklabels([''] + class_name)
-    ax.set_yticklabels([''] + class_name)
-    plt.xlabel
-    plt.savefig('/content/ex/workspace/test_confusion_matrix', dpi =300)
-    test_loss_list.append(epoch_loss)
-    test_acc_list.append(epoch_acc)
-    print('-'*30)
-    print('>> test | Loss : {:.4f}  Acc : {:.4f}'.format(epoch_loss,epoch_acc))
-
-test()
+#testing
+tester = utils.Tester(model,criterion,optimizer,test_data_loader)
+tester.test()
